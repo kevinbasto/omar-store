886790b3330ea022aa26fa2138065bc202e5bb7e
Kevin <bastokevindaniel@gmail.com>
2021-11-29 12:55:41 -0600

    roles endpoint created

--- src/endpoints/roles/entities/role.entity.ts
+++ src/endpoints/roles/entities/role.entity.ts
@@ -1,1 +1,11 @@
-export class Role {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Role {
+    @PrimaryGeneratedColumn('identity')
+    roleId : number;
+    @Column({length: 99})
+    name : string;
+    @Column('int')
+    authority : number;
+}
--- src/endpoints/roles/roles.service.ts
+++ src/endpoints/roles/roles.service.ts
@@ -1,27 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { Role } from './entities/role.entity';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    console.log(createRoleDto);
-    return 'This action adds a new role';
+
+  constructor(
+    @Inject('ROLE_REPOSITORY') private database : Repository<Role>
+  ){}
+
+  async create(createRoleDto: CreateRoleDto) {
+    try {
+      await this.database.save(createRoleDto);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+    return 'role created';
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll() {
+    let roles = await this.database.find()
+    roles.map((role : any) => {
+      role.roleId = parseInt(role.roleId);
+      role.authority = parseInt(role.authority);
+      return role;
+    })
+    return roles;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number) {
+    let role : Role;
+    role = await this.database.findOne(id)
+    .then((res : any) => { 
+      res.roleId = parseInt(res.roleId); 
+      res.authority = parseInt(res.authority); 
+      return res; 
+    });
+    return role;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    try {
+      await this.database.update(id, updateRoleDto);
+    } catch (error) {
+      throw error;
+    }
+    return `Role #${id} updated`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    try {
+      await this.database.delete(id);
+    } catch (error) {
+      throw error;
+    }
+    return `Role #${id} deleted`;
   }
 }
