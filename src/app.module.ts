50585545f4cb2704c361950296246c14e13b2d33
Kevin <bastokevindaniel@gmail.com>
2021-11-29 18:29:21 -0600

    generated product resources;

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { RolesModule } from './endpoints/roles/roles.module';
 import { UsersModule } from './endpoints/users/users.module';
 import { AccountsModule } from './endpoints/accounts/accounts.module';
+import { ProductModule } from './endpoints/product/product.module';
 
 @Module({
-  imports: [RolesModule, UsersModule, AccountsModule],
+  imports: [RolesModule, UsersModule, AccountsModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
